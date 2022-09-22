4e9df84373fb797bf99cf3f4d19474bfe7ec5135
Faith Makinde <faithmakinde0@gmail.com>
2022-09-22 14:45:14 +0100

    work in progress

--- recent.js
+++ recent.js
@@ -35,3 +35,21 @@ count6El.addEventListener("click", function(){
     like6.textContent = count6;
    }
   
+   let count7El = document.getElementById("count-7");
+   let like7 = document.getElementById("like-7")
+   let count7 = 1;
+   let save = JSON.parse(localStorage.getItem("count7")) ;
+
+   if(save){
+    count7 = save;
+    renderOut()
+  }
+  count7El.addEventListener("click", function(){
+      count7++;
+      localStorage.setItem("count7", JSON.stringify(count7));
+      renderOut()
+ })
+
+   function renderOut(){
+    like7.textContent = count7;
+   }
